5548a4d3c9771fb0a0889870392d7f27919ba855
SungMinKim <>
2019-02-23 20:15:58 +0900

    fourth commit

--- mapblog/projectMapBlog/views.py
+++ mapblog/projectMapBlog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import random
 
 def index(request):
-    return render(request, 'index.html')
-# Create your views here.
+    cafeteria = ["haru", "cow_gop"]
+    xCafeteria = ["37.558486", "37.558715"]
+    yCafeteria = ["126.935894", "126.935051"]
+    select_cafeteria = random.choice(cafeteria)
+    index_num = cafeteria.index(select_cafeteria)
+    x = xCafeteria[index_num]
+    y = yCafeteria[index_num]
+    return render(request, 'index.html', {'cafeteria' : select_cafeteria, 'x' : x, 'y' : y})
