276a9ff2858e9d866543fca026dbdeacd629d51d
SungMinKim <>
2019-02-23 19:16:37 +0900

    third commit

--- mapblog/mapblog/urls.py
+++ mapblog/mapblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import projectMapBlog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', projectMapBlog.views.index, name="index"),
 ]
