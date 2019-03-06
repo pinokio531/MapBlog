9c6824da1b6410fb9e8a03d11ce92c73c2be1dee
SungMinKim <>
2019-03-06 13:45:57 +0900

    modify

--- mapblog/mapblog/settings.py
+++ mapblog/mapblog/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '82=3ce#(9_0k*(c(7n4d3wewf!7kdjf18dp4inz71k+#l3_vg2'
+# SECRET_KEY = '82=3ce#(9_0k*(c(7n4d3wewf!7kdjf18dp4inz71k+#l3_vg2'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '82=3ce#(9_0k*(c(7n4d3wewf!7kdjf18dp4inz71k+#l3_vg2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
